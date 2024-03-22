4c5f802d4dfd11e1605cb56879f930453f0c1353
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:22:47 -0700

    Students Grades

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+let marks = 49;
+if (marks >= 90)
+    console.log("Grade A1");
+if (marks >= 80 || marks < 90)
+    console.log("Grade A");
+if (marks >= 70 || marks < 80)
+    console.log("Grade B");
+if (marks >= 60 || marks < 70)
+    console.log("Grade C");
+if (marks >= 50 || marks < 60)
+    console.log("Grade D");
+else
+    ;
+console.log("You are fail");
---
+++ main.ts
@@ -0,0 +1,18 @@
+// IF or ELSE ConditioN
+//logical operators [ OR => || ]
+//GRADES OF STUDENT
+let marks = 49
+if (marks >= 90)
+    console.log ("Grade A1");
+
+if (marks >= 80 || marks < 90)
+    console.log ("Grade A")
+if (marks >= 70 || marks < 80)
+    console.log ("Grade B")
+if (marks >= 60 || marks < 70)
+    console.log ("Grade C")    
+if (marks >= 50 || marks < 60) 
+    console.log ("Grade D")
+else ;
+    console.log ("You are fail")
+
